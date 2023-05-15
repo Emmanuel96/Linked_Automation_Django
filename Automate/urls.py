9de3bf29b2b66c30edaa135e2cd68645d2c7dc59
Emmanuel <kole.audu@gmail.com>
2023-05-15 19:28:36 +0100

    update

--- Automate/urls.py
+++ Automate/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.connect,),
     path('apply/', views.apply, name='apply'),
     path('connect/', views.connect, name="connect")
 ]
